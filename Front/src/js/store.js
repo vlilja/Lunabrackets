4d7bed0ed22e6231cfdc29e2b485a86d1aa4a630
Valtteri Lilja <valtteri.lilja@gmail.com>
2018-03-06 18:14:08 +0000

    added logger

--- Front/src/js/store.js
+++ Front/src/js/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
 
 import reducers from './reducers';
 
-const middleware = applyMiddleware(promise(), thunk);
+const middleware = applyMiddleware(promise(), thunk, logger());
 
 export default createStore(reducers, middleware);
