62cc4dcc29c93ccdabed0d8b941d672b29b9e439
Otto Paaso <otto.paaso@reaktor.com>
2018-03-29 20:49:49 +0300

    Bring submit function as is

--- Front/src/js/components/TournamentResultsFormTest.js
+++ Front/src/js/components/TournamentResultsFormTest.js
@@ -1,8 +1,33 @@
 import chai from 'chai';
 const {expect} = chai
 
+const submit = () => {
+  const { pickedPlayers } = this.state;
+  if (pickedPlayers.length <= 16) {
+    const places = [];
+    const rankedPlayers = [];
+    pickedPlayers.forEach((p, idx) => {
+      places.push(idx + 1);
+      rankedPlayers.push({ playerId: p.id, place: p.ranking });
+    });
+    rankedPlayers.sort((a, b) => a.place - b.place);
+    let valid = true;
+    for (let i = 0; i < rankedPlayers.length; i += 1) {
+      if (places[i] !== rankedPlayers[i].place) {
+        valid = false;
+      }
+    }
+    if (valid) {
+      console.log(rankedPlayers);
+      this.props.createTournamentResults(rankedPlayers);
+    } else {
+      this.setState({ error: true });
+    }
+  }
+}
+
 describe('Tournament Result Form validation', () => {
-    it('Validates players before submitting results', () => {
-        expect(true).to.eql(true)
-    })
+  it('Validates players before submitting results', () => {
+      expect(true).to.eql(true)
+  })
 });
