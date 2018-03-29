bc8e1c92beaf945ba94bf17eac77fd17c33e3d8c
Otto Paaso <otto.paaso@reaktor.com>
2018-03-30 00:14:21 +0300

    Use a precondition check to check for too many players
    Avoid nesting the whole function

--- Front/src/js/components/TournamentResultsForm.jsx
+++ Front/src/js/components/TournamentResultsForm.jsx
@@ -4,28 +4,30 @@ import _ from 'lodash';
 import phrases from '../../Phrases';
 
 export function validate(pickedPlayers) {
-  if (pickedPlayers.length <= 16) {
-    const places = [];
-    const rankedPlayers = [];
-    pickedPlayers.forEach((p, idx) => {
-      places.push(idx + 1);
-      rankedPlayers.push({ playerId: p.id, place: p.ranking });
-    });
-    rankedPlayers.sort((a, b) => a.place - b.place);
-    let valid = true;
-    for (let i = 0; i < rankedPlayers.length; i += 1) {
-      if (places[i] !== rankedPlayers[i].place) {
-        valid = false;
-      }
+  if (pickedPlayers.length > 16) {
+    return undefined
+  }
+
+  const places = [];
+  const rankedPlayers = [];
+  pickedPlayers.forEach((p, idx) => {
+    places.push(idx + 1);
+    rankedPlayers.push({ playerId: p.id, place: p.ranking });
+  });
+  rankedPlayers.sort((a, b) => a.place - b.place);
+  let valid = true;
+  for (let i = 0; i < rankedPlayers.length; i += 1) {
+    if (places[i] !== rankedPlayers[i].place) {
+      valid = false;
     }
-    if (valid) {
-      console.log(rankedPlayers);
-      // this.props.createTournamentResults(rankedPlayers);
-      return rankedPlayers;
-    } else {
-      // this.setState({ error: true });
-      return undefined
-    }
+  }
+  if (valid) {
+    console.log(rankedPlayers);
+    // this.props.createTournamentResults(rankedPlayers);
+    return rankedPlayers;
+  } else {
+    // this.setState({ error: true });
+    return undefined
   }
 }
 
