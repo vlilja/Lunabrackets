1d66d21b1a7e2298645a161e047f683e345589f9
Otto Paaso <otto.paaso@reaktor.com>
2018-03-30 00:07:17 +0300

    Add succesful test case

--- Front/src/js/components/TournamentResultsFormTest.js
+++ Front/src/js/components/TournamentResultsFormTest.js
@@ -41,4 +41,20 @@ describe('Tournament Result Form validation', () => {
   it('Returns empty array when given an empty array', () => {
     expect(validate([])).to.eql([])
   })
+
+  it('Verifies that players are given rankings through 1 - <playerCount>', () => {
+    const pickedPlayers = [
+      { id: 0, ranking: 1},
+      { id: 2, ranking: 3},
+      { id: 1, ranking: 2},
+      { id: 3, ranking: 4}
+    ]
+
+    expect(validate(pickedPlayers)).to.eql([
+      { playerId: 0, place: 1},
+      { playerId: 1, place: 2},
+      { playerId: 2, place: 3},
+      { playerId: 3, place: 4}
+    ])
+  })
 });
