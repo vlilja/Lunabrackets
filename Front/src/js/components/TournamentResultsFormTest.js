c39a5a8351beb4903cc34ab0fa0406b6a1161b8e
Otto Paaso <otto.paaso@reaktor.com>
2018-03-29 20:47:31 +0300

    Add dummy test for Front, run with npm run test

---
+++ Front/src/js/components/TournamentResultsFormTest.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const {expect} = chai
+
+describe('Tournament Result Form validation', () => {
+    it('Validates players before submitting results', () => {
+        expect(true).to.eql(true)
+    })
+});
